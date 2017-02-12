5f54d6ea0a4e374f47cc590b40176937df7352cf
augustolazaro <aemlazaro@gmail.com>
2017-02-11 23:23:20 -0200

    Site updated to 7a05c76
    [skip ci]

---
+++ javascripts/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $(".menu-toggler").on("click", function(){
+        $(".navigation").toggleClass("show-menu");
+    });
+
+    var slider = document.querySelector(".Wallop");
+    var wallop = new Wallop(slider);
+
+});
